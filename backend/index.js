6e82b89a8afa97dd193de53104f1bc1ea6e3dc96
Yash <yashvardhan983@gmail.com>
2024-11-30 23:01:41 +0530

    get added

--- backend/index.js
+++ backend/index.js
@@ -10,18 +10,32 @@ const routes = require("./routes");
 connectDb();
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://flavoro-mern.netlify.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://flavoro-mern.netlify.app",
+      "http://localhost:5000",
+    ],
     credentials: true,
   })
 );
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173','https://flavoro-mern.netlify.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173",
+    "https://flavoro-mern.netlify.app",
+    "http://localhost:5000"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
+
+// Define a route for the root endpoint
+app.get("/", (req, res) => {
+  res.send("Server is running on the front page!");
+});
 
 app.use("/api", routes);
 
