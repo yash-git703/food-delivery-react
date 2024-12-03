fdc587c2ce7d87d0fd5f6ac6d09e55d19d1172cd
Yash <yashvardhan983@gmail.com>
2024-12-03 19:47:55 +0530

    preload final

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,34 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ResetPassword from "./pages/ResetPassoword";
 import VerifyOtp from "./pages/VerifyOtp";
-import Preloader from "./components/Preloader"
+// import Preloader from "./components/Preloader"
 
 function App() {
 
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Check if the preloader has already been shown
-    const preloaderShown = localStorage.getItem("preloaderShown");
+  // useEffect(() => {
+  //   // Check if the preloader has already been shown
+  //   const preloaderShown = localStorage.getItem("preloaderShown");
 
-    if (preloaderShown) {
-      // If the preloader was already shown, skip it
-      setLoading(false);
-    } else {
-      // Otherwise, show the preloader
-      const timer = setTimeout(() => {
-        setLoading(false);
-        // Mark the preloader as shown in local storage
-        localStorage.setItem("preloaderShown", "true");
-      }, 5000); // 5-second preloader
+  //   if (preloaderShown) {
+  //     // If the preloader was already shown, skip it
+  //     setLoading(false);
+  //   } else {
+  //     // Otherwise, show the preloader
+  //     const timer = setTimeout(() => {
+  //       setLoading(false);
+  //       // Mark the preloader as shown in local storage
+  //       localStorage.setItem("preloaderShown", "true");
+  //     }, 5000); // 5-second preloader
 
-      return () => clearTimeout(timer); // Cleanup the timer
-    }
-  }, []);
+  //     return () => clearTimeout(timer); // Cleanup the timer
+  //   }
+  // }, []);
 
-  if (loading) {
-    return <Preloader />; // Render the preloader only if loading
-  }
+  // if (loading) {
+  //   return <Preloader />; // Render the preloader only if loading
+  // }
 
   
   return (
